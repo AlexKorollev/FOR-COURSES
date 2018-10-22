cf1a8074b2a954860693e1d64e5da8ef80d63c5c
Alex <xbox360-11@mail.ru>
2018-10-22 12:56:44 +0300

    Complete #1task
    - add functional to the third button
    - add css for the third button

--- js/js.js
+++ js/js.js
@@ -1,4 +1,21 @@
-
 second.onclick = function(){
-    body.style.background = "#2d2d2d";
+    content.style.background = "#2d2d2d";
+
+
+    // var change = document.getElementById("content");
+    // if (change.style.background == "#bcbcbc")
+    //     change.style.background = "#2d2d2d";
+    // else
+    //     change.style.background = "#bcbcbc";
+
+
+    // change.style.background = (change.style.background == "#2d2d2d")?"#bcbcbc":"#2d2d2d";
+}
+
+third.onclick = function(){
+    body.removeChild(content);
+    var div = document.createElement('div');
+    div.className = "second-content";
+    div.innerHTML = "<div class='block'>Новый блок №1</div><div class='block'>Новый блок №2</div><div class='block'>Новый блок №3</div>";
+    body.appendChild(div);
 }
